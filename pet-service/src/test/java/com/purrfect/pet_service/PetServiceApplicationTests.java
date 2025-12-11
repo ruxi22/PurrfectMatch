44916453b9479a55d43c51132c50513794e0fecd
ruxi22 <ruxandrapopa22@yahoo.com>
2025-12-12 00:03:40 +0200

    unit tests 7

--- pet-service/src/test/java/com/purrfect/pet_service/PetServiceApplicationTests.java
+++ pet-service/src/test/java/com/purrfect/pet_service/PetServiceApplicationTests.java
@@ -1,21 +1,25 @@
 package com.purrfect.pet_service;
 
-import com.purrfect.pet_service.domain.Pet;
-import com.purrfect.pet_service.domain.PetType;
+import com.purrfect.pet_service.controller.PetController;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-class PetServiceApplicationTests {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@WebMvcTest(PetController.class)
+class PetControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void petShouldStoreAndReturnValuesCorrectly() {
-		Pet pet = new Pet();
-		pet.setName("Milo");
-		pet.setAge(2);
-		pet.setType(PetType.CAT);
-
-		assertEquals("Milo", pet.getName());
-		assertEquals(2, pet.getAge());
-		assertEquals(PetType.CAT, pet.getType());
+	void pingShouldReturnOk() throws Exception {
+		mockMvc.perform(get("/ping"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("pet-service-ok"));
 	}
 }
