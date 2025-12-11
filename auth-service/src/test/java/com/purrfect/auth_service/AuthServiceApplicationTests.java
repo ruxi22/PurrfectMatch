11feec57f4915aecf6fc769e996057dc87cb28a6
ruxi22 <ruxandrapopa22@yahoo.com>
2025-12-11 23:38:38 +0200

    unit tests 3

--- auth-service/src/test/java/com/purrfect/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/purrfect/auth_service/AuthServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package com.purrfect.auth_service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class AuthServiceApplicationTests {
+class PasswordEncodingTest {
 
 	@Test
-	void contextLoads() {
-	}
+	void passwordShouldBeEncodedCorrectly() {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
+		String raw = "mySecret123";
+		String encoded = encoder.encode(raw);
+
+		assertNotEquals(raw, encoded, "Encoded password should not match raw password");
+		assertTrue(encoder.matches(raw, encoded), "Encoder should verify encoded password");
+	}
 }
